ae888fa762d416f25b85c183b7a7ca2e0c750c28
elbjek <lenabjekovic@hotmaill.com>
2018-06-03 14:35:17 +0200

    sortet by timestamp

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,83 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+
+
+  <div class="notification">
+<div id="single-notification" v-if="show" v-for="item in notifications"
+      v-bind:item="item"
+      v-bind:key="item.id"
+      class="single-notification">
+  <img v-bind:src="item.icon"  alt=""> 
+  <p>{{item.message}}</p>
+  <p @click="showNotifications()" >{{item.timestamp}}</p>
+</div>
+
+
+  
+
+
   </div>
 </template>
 
 <script>
+
+import axios from 'axios';
+import moment,{ isDuration } from 'moment';
+
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+   
+  },
+  data(){
+return{
+  notifications:[],
+  timestamps:[],
+  show:true
+}
+  },
+    mounted(){
+    axios.get('https://api.myjson.com/bins/19wyhe')
+  .then(response => (this.notifications = response.data))
+  .catch(function (error) {
+    console.log(error);
+  })
+
+  },
+  methods:{
+    showNotifications(){
+      var arraySort = require('array-sort');
+        arraySort(this.notifications, 'timestamp');
+
+ },
+
+    
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style scoped   lang='scss'>
+
+.notification{
+  max-width:500px;
+  background-color: pink;
+  margin:0 auto;
+  .single-notification{
+    display:flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: row;
+    border:1px solid black;
+    padding:10px;
+    
+    img{
+      height: 30px;
+    }
+  }
+  .gray{
+    background-color: gray;
+  }
 }
 </style>
